9a29be59a27c37ec29e79b32edc0dd19c0d09800
Bastien Colmard <baztoune@gmail.com>
2013-03-17 19:21:50 +0100

    adde cart to header

--- wp-content/themes/kelontongfree/header.php
+++ wp-content/themes/kelontongfree/header.php
@@ -57,6 +57,9 @@
 	</nav>
 
 	<header id="header">
+		<div style="float:right;">
+			<?php echo do_shortcode('[shopping_cart empty_msg="Panier vide"]'); ?>
+		</div>
 		<hgroup>
 			<h1 id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="tagline"><?php bloginfo( 'description' ); ?></h2>
